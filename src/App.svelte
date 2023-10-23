fa17aff90794040622cfa809779ce7d97cc64e08
Amund <Lullinj98@gmail.com>
2023-10-23 15:57:58 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Item from "./lib/ItemCard.svelte";
+
+  const items = ["dragon lore", "M9 brødretta", "Nova", "Nova sniper"]
+</script>
+
+<main>
+  <h1>Demo</h1>
+  {#each items as item}
+    <Item item={item}/>
+  {/each}
+</main>
+
+<style>
+</style>
---
+++ src/lib/ItemCard.svelte
@@ -0,0 +1,19 @@
+<script>
+import demo from "../assets/demo.png";
+export let item;
+</script>
+
+<div>
+    <img src={demo} alt="Demo"/>
+    <h2>{item}</h2>
+    <p>Dette er demo tekst</p>
+</div>
+
+<style>
+    div {
+        display: inline-block;
+        border: black solid 1px;
+        margin-right: 10px;
+        padding: 10px;
+    }
+</style>
